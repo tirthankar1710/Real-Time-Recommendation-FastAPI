2faf0b0fe2225b056d2f968ad52dbe0f1bc79861
Tirthankar Ghosh <tirthankar@Tirthankars-Mac-mini.local>
2025-01-30 01:33:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, BackgroundTasks
+import pandas as pd
+from surprise import dump
+
+app = FastAPI()
+
+
+# Need to fetch model artifacts on start up 
+# Need to fetch on reload based.
+
+@app.get("/")
+def read_root():
+    return {"Status": "Running!"}
+
+@app.get("/test_function")
+def read_root():
+    return {"Status": "Test Function Running!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ utils.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import yaml
+from logging_util import logger
+from box import ConfigBox
+from box.exceptions import BoxValueError
+
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
